9d32a25e627c8441ec05f627f6166c6d82a74593
davidrguez98 <ropeda98@gmail.com>
2025-01-20 20:49:36 +0100

    La estructura del proyecto creo que está hecha

--- src/app.py
+++ src/app.py
@@ -1,55 +1,47 @@
-while True:
+class Task:
 
-    tasks = []
+    def __init__(self, task, description):
+        self.task = task
+        self.description = description
 
-    def new_task():
-        task = str(input("¿Qué tarea quieres añadir?: "))  
-        if task in tasks:
-            qt = str(input(f"La tarea {task} ya se encuentra en la lista de tareas. ¿Desea añadirla?: "))
-            if qt.lower() == "si":
-                print(f"La tarea {task} ha sido añadida correctamente.")
-                tasks.append(task)
-            else:
-                print(f"La tarea {task} no ha sido añadida.")                   
-        if task not in tasks:
-            print(f"La tarea {task} ha sido añadida correctamente")
-            tasks.append(task)
-            print(tasks)
+class ToDo:
+
+    def __init__(self):
+        self.tasks = []
+
+    def show_TasksList(self):
+        if not self.tasks:
+            print("No hay tareas en la lista.")
+        else:
+            print("Lista de tareas:")
+            for task in self.tasks:
+                print(f"    - {task["Task"]}: {task["Description"]}")
         
+    def new_task(self):
+        task = str(input("Nombre de la tarea: "))
+        description = str(input("Descripción de la tarea: "))
+        new_task = {"Task": task.capitalize(), "Description": description.capitalize()}
+        self.tasks.append(new_task)
+        print(f"La tarea {task} ha sido creada correctamente.")
 
-    print("")
-    print("1. Ver lista de tareas.")
-    print("2. Añadir una tarea.")
-    print("3. Actualizar una tarea de la lista de tareas.")
-    print("4. Eliminar una tarea de la lista de tareas.")
-    print("5. Salir del programa.")
-    index = str(input("\nEscribe la opción que deseas realizar: "))
+    def delete_task(self):
+        
+        self.show_TasksList()
+        
+        a = str(input("¿Qué tarea quieres eliminar?: "))
+        for task in self.tasks:
+            if self.tasks["Task"] == a.capitalize(): #No me hace bien la comparación.
+                self.tasks.remove(task)
+                print(f"La tarea {a} ha sido eliminada correctamente.")
+                return
+        print(f"La tarea {a} no se encuentra en la lista")
 
-    match index:
-        case "1":
-            print(tasks)
-        case "2":
-            new_task()
-        case "3":
-            def update_task():
-                print(tasks)
-                task = str(input("¿Qué tarea quieres actualizar?"))
-                if task in tasks:
-                    del tasks[task]
-                    new_task()
-                    print(f"La tarea {task} ha sido actualizada.")
-                else:
-                    print(f"La tarea {task} no se encuentra en la lista de tareas.")
-        case "4":
-            def delete_task():
-                print(tasks)
-                task = str(input("¿Qué tarea quieres eliminar?: "))
-                if task.capitalize() in tasks:
-                    print(f"La tarea {task} ha sido eliminada")
-                    del tasks[task]
-                else:
-                    print(f"La tarea {task} no se encuentra en la lista de tareas.")
-        case "5":
-            break
-        case "6":
-            print("\nLa opción marcada no es correcta. Selecciona un número del 1 al 5.")
+
+todo = ToDo()
+
+todo.show_TasksList()
+todo.new_task()
+todo.new_task()
+todo.new_task()
+todo.delete_task()
+todo.show_TasksList()
