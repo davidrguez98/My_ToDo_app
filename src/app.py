15f299aa8d4416e9443c98317d237f7fcdf0016e
davidrguez98 <ropeda98@gmail.com>
2025-01-21 20:29:53 +0100

    Creo qeu por fin estoy encaminando todo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,13 @@
-tasks = {}
+""" tasks = {
+    "ID" : 0,
+    "Task" : "",
+    "Description": "",
+    "Status": "",
+    "Created date": "",
+    "Updated date": "",
+} """
+
+tasks = []
 
 def tasks_list():
     if not tasks:
@@ -7,17 +16,20 @@ def tasks_list():
         print(tasks)
 
 def new_task():
-    task = input("Tarea: ")
-    description = input("Descripción: ")
-    tasks[task.capitalize()] = description.capitalize()
-    print(f"La tarea {task} ha sido creada.")
+    task = input("Tarea: ").capitalize()
+    description = input("Descripción: ").capitalize()
+    status = input("Eligen entre: todo, in-progress o done: ").capitalize()
+    created_date = "yesterday".capitalize()
+    updated_date = "today".capitalize()
+    tasks.append({"Task": task, "Description": description, "Status": status, "Created date": created_date, "Update date": updated_date})
 
 def delete_task():
     task = input("Eliminar: ").capitalize()
-    if task in tasks:
-        del tasks[task]
-    else:
-        (f"La tarea {task} no se encuentra en la lista.")
+    for value in tasks.items():
+        if value == task:
+            del tasks[task]
+        else:
+            (f"La tarea {task} no se encuentra en la lista.")
         
 def refresh_task():
     task = input("¿Qué tarea quieres actualizar?: ").capitalize()
@@ -32,10 +44,9 @@ def refresh_task():
 
 
 
+tasks_list()
 new_task()
 new_task()
 tasks_list()
 delete_task()
-tasks_list()
-refresh_task()
 tasks_list()
